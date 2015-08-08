8a3bc7f714c2e8705a08b0c2e18705c4101fdb7c
Roman <romnempire@gmail.com>
2015-08-08 19:22:21 -0400

    clean up resources.js

--- routes/resources.js
+++ routes/resources.js
@@ -5,45 +5,16 @@ router.get('/', function(req, res) {
   var resources = {
     tabs: ['work', 'housing', 'food', 'education', 'transportation'],
     defaultTab: 'work',
-    resources: [{
-        id: 'catresource',
-        name: 'free dogs',
-        address: '101 Meowton Ave.',
-        createDate: new Date(),
-        comments: [{
-            body: 'they taste good',
-            createDate: new Date()
-          }
-        ]
-      }
-    ]
+    resources: []
   };
 
   var collection = req.db.get('resources');
   collection.find({},{}, function(e, data) {
     console.log(data);
 
-    resources.resources[0].id = data[0]._id;
-    resources.resources[0].name = data[0].name;
-    resources.resources[0].address = data[0].address;
-
+    resources.resources = data;
     res.render('resources', resources);
   });
 });
 
 module.exports = router;
-
-
-/*
-function(err, data) {
-
-    console.log('cats');
-    console.log(data);
-
-    resources.resources[0].id = data[0]._id;
-    resources.resources[0].name = data[0].name;
-    resources.resources[0].address = data[0].address;
-
-    res.render('resources', resources);
-  }
-*/
