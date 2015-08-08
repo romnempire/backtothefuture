bc033c53d8b287371aa2bddabeeb80d9b0d2f2b4
Roman <romnempire@gmail.com>
2015-08-08 14:28:02 -0400

    building resources route

--- app.js
+++ app.js
@@ -12,6 +12,7 @@ var db = monk('localhost:27017/backtothefuture');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
+var resources = require('./routes/resources');
 
 var app = express();
 
@@ -35,6 +36,7 @@ app.use(function(req,res,next) {
 
 app.use('/', routes);
 app.use('/users', users);
+app.use('/resources', resources);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
