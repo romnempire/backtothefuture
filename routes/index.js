28ba1a8f874a553fd117e56c8bc64afbfca804d4
camwood23 <camwood23@gmail.com>
2015-08-08 14:55:06 -0400

    Added article section to pages

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,11 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/tutorial/:tagId', function(req, res) {
+  var collection = req.db.get('tutorialcollection');
+  collection.find({title: req.params.tagId}, {}, function(e, docs) {
+    res.render('page', { title: docs[0].title, body: docs[0].body });
+  });
+});
+
 module.exports = router;
