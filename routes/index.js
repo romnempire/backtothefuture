e514e5ec3f01b0b59c07d6b5ff822bf60566a307
camwood23 <camwood23@gmail.com>
2015-08-08 17:58:21 -0400

    Can now add comments

--- routes/index.js
+++ routes/index.js
@@ -9,10 +9,33 @@ router.get('/', function(req, res, next) {
 router.get('/tutorial/:tagId', function(req, res) {
   var collection = req.db.get('tutorialcollection');
   collection.find({title: req.params.tagId}, {}, function(e, docs) {
+    console.log(docs[0]._id);
     if (docs.length == 0) {
       res.send('Error, no page found');
     } else {
-    res.render('page', { title: docs[0].title, body: docs[0].body });
+      collection.find({relID: docs[0]._id}, {}, function(e, newDocs) {
+        res.render('page', { 
+          title: docs[0].title, 
+          body: docs[0].body, 
+          comments: newDocs,
+          pageID: docs[0]._id
+        });
+      });
+    }
+  });
+});
+
+router.post('/addcomment/:tagId', function(req, res) {
+  var collection = req.db.get('tutorialcollection');
+  var comment = req.body.comment
+  collection.insert({
+    "relID" : collection.id(req.params.tagId),
+    "text" : comment
+  }, function(err, doc) {
+    if (err) {
+      res.send("There was a problem");
+    } else {
+      res.redirect('/tutorial/howtocook');
     }
   });
 });
