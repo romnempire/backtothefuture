41d0b6f446a5cf572416fec2e09458c8845ed946
ZhengC1 <zhengc1@appstate.edu>
2015-08-08 16:32:17 -0400

    made a carousel and navbar

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,9 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/about', function(req, res, next) {
+   res.render('about', {title: 'BackToTheFuture' });
+});
+
+
 module.exports = router;
