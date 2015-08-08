c7a63d5ef507762fff4d53d518a0e323de4d46ff
Roman <romnempire@gmail.com>
2015-08-08 17:03:24 -0400

    db connexions

--- routes/resources.js
+++ routes/resources.js
@@ -7,8 +7,8 @@ router.get('/', function(req, res) {
     defaultTab: 'work',
     resources: [{
         id: 'catresource',
-        name: 'free cats',
-        address: '101 Dalmation Ln.',
+        name: 'free dogs',
+        address: '101 Meowton Ave.',
         createDate: new Date(),
         comments: [{
             body: 'they taste good',
@@ -18,7 +18,32 @@ router.get('/', function(req, res) {
       }
     ]
   };
-  res.render('resources', resources);
+
+  var collection = req.db.get('resources');
+  collection.find({},{}, function(e, data) {
+    console.log(data);
+
+    resources.resources[0].id = data[0]._id;
+    resources.resources[0].name = data[0].name;
+    resources.resources[0].address = data[0].address;
+
+    res.render('resources', resources);
+  });
 });
 
 module.exports = router;
+
+
+/*
+function(err, data) {
+
+    console.log('cats');
+    console.log(data);
+
+    resources.resources[0].id = data[0]._id;
+    resources.resources[0].name = data[0].name;
+    resources.resources[0].address = data[0].address;
+
+    res.render('resources', resources);
+  }
+*/
