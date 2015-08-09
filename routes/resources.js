f7a3dfcacecf3e616204a49c0d85d7e77828d411
Roman <romnempire@gmail.com>
2015-08-09 02:04:40 -0400

    ajax comments for the resources

--- routes/resources.js
+++ routes/resources.js
@@ -4,16 +4,32 @@ var router = express.Router();
 router.get('/', function(req, res) {
   var resources = {
     tabs: ['work', 'housing', 'food', 'education', 'transportation', 'help'],
-    defaultTab: 'work',
+    defaultTab: 'help',
     resources: []
   };
 
   var collection = req.db.get('resources');
-  collection.find({},{}, function(e, data) {
-    console.log(data);
-
+  collection.find({type: resources.defaultTab},{}, function(e, data) {
     resources.resources = data;
+    //console.log(resources);
     res.render('resourcespage', resources);
+  });
+});
+
+router.post('/addcomment/:tagId', function(req, res) {
+  var collection = req.db.get('resources');
+  var id = collection.id(req.params.tagId);
+  console.log("adding comment");
+  console.log(req.body);
+  collection.insert({
+    resourceID: id,
+    text: req.body.commentText
+  }, function(err, data) {
+    if (err) {
+      res.send('gg');
+    } else {
+      res.send(data);
+    }
   });
 });
 
