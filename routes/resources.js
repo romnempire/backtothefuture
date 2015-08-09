89b1ae81e49a2c130bb56dbe043445b20029168c
Roman <romnempire@gmail.com>
2015-08-09 03:05:59 -0400

    comments view working

--- routes/resources.js
+++ routes/resources.js
@@ -10,13 +10,28 @@ router.get('/', function(req, res) {
 
   var collection = req.db.get('resources');
   collection.find({type: resources.defaultTab},{}, function(e, data) {
+    collection = req.db.get('resourcecomments');
     resources.resources = data;
-    res.render('resourcespage', resources);
+    accumulate('cat', resources.resources[0], 1);
   });
+
+  var accumulate = function(e, datum, nextIndex) {
+    if (nextIndex-1 >= resources.resources.length) {
+      console.log(resources);
+      res.render('resourcespage', resources);
+    } else {
+      console.log('the following is what was passed in from the inside');
+      console.log(datum);
+      collection.find({resourceID: resources.resources[0]._id}, {}, function(err, data) {
+        resources.resources[nextIndex-1].comments = data;
+        accumulate(e, data[nextIndex], nextIndex+1 );
+      });
+    }
+  }
 });
 
 router.post('/addcomment/:tagId', function(req, res) {
-  var collection = req.db.get('resources');
+  var collection = req.db.get('resourcecomments');
   var id = collection.id(req.params.tagId);
   console.log("adding comment");
   console.log(req.body);
